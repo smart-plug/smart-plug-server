374f3392dea2ac2d7401834553e3a010a7c9501e
pierresousa <pierrevssousa@gmail.com>
2023-05-07 10:37:07 -0300

    connection with database in function

---
+++ src/utils/bdConnection.ts
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const connectDB = async () => {
+  await mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.uheqdom.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`);
+};
+
+export default connectDB;
